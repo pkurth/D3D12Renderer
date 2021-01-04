50da1c2350181ccd19083b49e82fb3b6b87584f0
Philipp Kurth <philipp.kurth@fau.de>
2021-01-04 18:47:14 +0100

    Overloads for skinning

--- src/skinning.cpp
+++ src/skinning.cpp
@@ -18,7 +18,7 @@ static dx_pipeline skinningPipeline;
 struct skinning_call
 {
 	ref<dx_vertex_buffer> vertexBuffer;
-	submesh_info submesh;
+	vertex_range range;
 	uint32 jointOffset;
 	uint32 numJoints;
 	uint32 vertexOffset;
@@ -47,7 +47,7 @@ void initializeSkinning()
 	skinningMatrices.reserve(MAX_NUM_SKINNING_MATRICES_PER_FRAME);
 }
 
-std::tuple<ref<dx_vertex_buffer>, submesh_info, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, submesh_info submesh, uint32 numJoints)
+std::tuple<ref<dx_vertex_buffer>, vertex_range, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, vertex_range range, uint32 numJoints)
 {
 	uint32 offset = (uint32)skinningMatrices.size();
 
@@ -58,22 +58,40 @@ std::tuple<ref<dx_vertex_buffer>, submesh_info, mat4*> skinObject(const ref<dx_v
 	calls.push_back(
 		{
 			vertexBuffer,
-			submesh,
+			range,
 			offset,
 			numJoints,
 			totalNumVertices
 		}
 	);
 
+	vertex_range resultRange;
+	resultRange.numVertices = range.numVertices;
+	resultRange.firstVertex = totalNumVertices;
+
+	totalNumVertices += range.numVertices;
+
+	return { skinnedVertexBuffer[currentSkinnedVertexBuffer], resultRange, skinningMatrices.data() + offset };
+}
+
+std::tuple<ref<dx_vertex_buffer>, uint32, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, uint32 numJoints)
+{
+	auto [vb, range, mats] = skinObject(vertexBuffer, vertex_range{ 0, vertexBuffer->elementCount }, numJoints);
+
+	return { vb, range.firstVertex, mats };
+}
+
+std::tuple<ref<dx_vertex_buffer>, submesh_info, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, submesh_info submesh, uint32 numJoints)
+{
+	auto [vb, range, mats] = skinObject(vertexBuffer, vertex_range{ submesh.baseVertex, submesh.numVertices }, numJoints);
+
 	submesh_info resultInfo;
 	resultInfo.firstTriangle = submesh.firstTriangle;
 	resultInfo.numTriangles = submesh.numTriangles;
-	resultInfo.numVertices = submesh.numVertices;
-	resultInfo.baseVertex = totalNumVertices;
+	resultInfo.baseVertex = range.firstVertex;
+	resultInfo.numVertices = range.numVertices;
 
-	totalNumVertices += submesh.numVertices;
-
-	return { skinnedVertexBuffer[currentSkinnedVertexBuffer], resultInfo, skinningMatrices.data() + offset };
+	return { vb, resultInfo, mats };
 }
 
 bool performSkinning()
@@ -98,9 +116,9 @@ bool performSkinning()
 		{
 			cl->setRootComputeSRV(SKINNING_RS_INPUT_VERTEX_BUFFER, c.vertexBuffer->gpuVirtualAddress);
 
-			cl->setCompute32BitConstants(SKINNING_RS_CB, skinning_cb{ c.jointOffset, c.numJoints, c.submesh.baseVertex, c.submesh.numVertices, c.vertexOffset });
+			cl->setCompute32BitConstants(SKINNING_RS_CB, skinning_cb{ c.jointOffset, c.numJoints, c.range.firstVertex, c.range.numVertices, c.vertexOffset });
 
-			cl->dispatch(bucketize(c.submesh.numVertices, 512));
+			cl->dispatch(bucketize(c.range.numVertices, 512));
 		}
 
 		cl->uavBarrier(skinnedVertexBuffer[currentSkinnedVertexBuffer]);
--- src/skinning.h
+++ src/skinning.h
@@ -4,6 +4,14 @@
 #include "geometry.h"
 #include "math.h"
 
+struct vertex_range
+{
+	uint32 firstVertex;
+	uint32 numVertices;
+};
+
 void initializeSkinning();
+std::tuple<ref<dx_vertex_buffer>, vertex_range, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, vertex_range range, uint32 numJoints);
+std::tuple<ref<dx_vertex_buffer>, uint32, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, uint32 numJoints);
 std::tuple<ref<dx_vertex_buffer>, submesh_info, mat4*> skinObject(const ref<dx_vertex_buffer>& vertexBuffer, submesh_info submesh, uint32 numJoints);
 bool performSkinning();
