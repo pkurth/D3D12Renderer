bc54994ad77f7afd1c4615978393873f551a4ee3
Philipp Kurth <philipp.kurth@fau.de>
2022-04-05 18:06:32 +0200

    Reverb header

---
+++ src/audio/reverb.h
@@ -0,0 +1,78 @@
+#pragma once
+
+
+
+
+enum reverb_preset
+{
+	reverb_none = -1,
+
+	reverb_preset_default,
+	reverb_preset_generic,
+	reverb_preset_padded_cell,
+	reverb_preset_room,
+	reverb_preset_bathroom,
+	reverb_preset_livingroom,
+	reverb_preset_stone_room,
+	reverb_preset_auditorium,
+	reverb_preset_concert_hall,
+	reverb_preset_cave,
+	reverb_preset_arena,
+	reverb_preset_hangar,
+	reverb_preset_carpeted_hallway,
+	reverb_preset_hallway,
+	reverb_preset_stone_corridor,
+	reverb_preset_alley,
+	reverb_preset_forest,
+	reverb_preset_city,
+	reverb_preset_mountains,
+	reverb_preset_quarry,
+	reverb_preset_plain,
+	reverb_preset_parking_lot,
+	reverb_preset_sewer_pipe,
+	reverb_preset_under_water,
+	reverb_preset_small_room,
+	reverb_preset_medium_room,
+	reverb_preset_large_room,
+	reverb_preset_medium_hall,
+	reverb_preset_large_hall,
+	reverb_preset_plate,
+
+	reverb_preset_count,
+};
+
+static const char* reverbPresetNames[] =
+{
+	"Default",
+	"Generic",
+	"Padded cell",
+	"Room",
+	"Bathroom",
+	"Livingroom",
+	"Stone room",
+	"Auditorium",
+	"Concert hall",
+	"Cave",
+	"Arena",
+	"Hangar",
+	"Carpeted hallway",
+	"Hallway",
+	"Stone corridor",
+	"Alley",
+	"Forest",
+	"City",
+	"Mountains",
+	"Quarry",
+	"Plain",
+	"Parking lot",
+	"Sewer pipe",
+	"Under water",
+	"Small room",
+	"Medium room",
+	"Large room",
+	"Medium hall",
+	"Large hall",
+	"Plate",
+};
+
+
