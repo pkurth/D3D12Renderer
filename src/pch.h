f6ceb438751c603bdd342f56f1468d4376e68f72
Philipp Kurth <philipp.kurth@fau.de>
2022-07-13 23:54:31 +0200

    Using proper sized types

--- src/pch.h
+++ src/pch.h
@@ -21,14 +21,14 @@ namespace fs = std::filesystem;
 
 #include <wrl.h> 
 
-typedef char int8;
-typedef unsigned char uint8;
-typedef short int16;
-typedef unsigned short uint16;
-typedef int int32;
-typedef unsigned int uint32;
-typedef long long int64;
-typedef unsigned long long uint64;
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
 typedef wchar_t wchar;
 
 template <typename T> using ref = std::shared_ptr<T>;
